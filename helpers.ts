30e8d4f42ac8f7c5a12ad7b2852b38e96818ac4d
charliieharti <138435301+charliieharti@users.noreply.github.com>
2026-03-20 09:24:21 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T06:24:14.015Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-20T06:24:21.536Z
+Commit: Fix memory leak in event handlers
