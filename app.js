5c8a8ac54a41baf83180b0992a3855a469445878
charliieharti <138435301+charliieharti@users.noreply.github.com>
2026-03-20 09:16:58 +0300

    Patch API endpoint error handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T06:16:15.081Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-20T06:16:58.440Z
+Commit: Patch API endpoint error handling
