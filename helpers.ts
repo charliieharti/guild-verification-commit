127f033daa00a4f4c658dfaf8e718cdfebef369b
charliieharti <138435301+charliieharti@users.noreply.github.com>
2026-03-20 09:23:28 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T06:23:18.557Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-20T06:23:29.078Z
+Commit: Resolve null pointer exception in user service
