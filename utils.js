5c34fac7050959a735ae9d1f3f7f2f8ce5abf4a8
charliieharti <138435301+charliieharti@users.noreply.github.com>
2026-03-20 09:23:32 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T06:23:25.678Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-20T06:23:32.344Z
+Commit: Fix race condition in async operations
