697bc380a3f9c71cfe123d8e134f940b163af80f
charliieharti <138435301+charliieharti@users.noreply.github.com>
2026-03-20 09:24:51 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T06:24:19.968Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-20T06:24:52.020Z
+Commit: Fix broken links in documentation
