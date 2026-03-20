128c6e6b610930b514f4e0e8754cbf159b69ec9a
charliieharti <138435301+charliieharti@users.noreply.github.com>
2026-03-20 09:24:02 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T06:23:58.959Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-20T06:24:02.496Z
+Commit: Fix broken links in documentation
