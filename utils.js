9cd72eb518d2d9c2a29a8d9bdc6d82eaa69811b6
charliieharti <138435301+charliieharti@users.noreply.github.com>
2026-03-20 09:25:36 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T06:25:33.189Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-20T06:25:36.287Z
+Commit: Fix CSS styling bug on mobile devices
